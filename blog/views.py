cb94afc6a2151c66dadaeaf8d0980fc9558ce73a
RUFAT RESUL <rufatresul997@gmail.com>
2023-06-20 18:46:35 +0400

    django_project

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# Create your views here.
+
+def searvices(request):
+    return render(request,'searvices.html')
+
+def searvices(request):
+    return render(request,'shop.html')
